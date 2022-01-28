c6759905e9992c68459581c7f983caeacee450e5
developervivek1 <vivekpathak.emmy1234@gmail.com>
2022-01-28 12:51:50 +0530

    passwrd

---
+++ js/script.js
@@ -0,0 +1,33 @@
+function myFunction(x) {
+    var x = document.getElementById("myInput");
+    x.classList.toggle("fa-thumbs-down");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+
+
+  function myFunction2(x) {
+    var x = document.getElementById("myInput2");
+    x.classList.toggle("fa-thumbs-down");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+
+
+  function myFunction3(x) {
+    var x = document.getElementById("myInput3");
+    x.classList.toggle("fa-thumbs-down");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
